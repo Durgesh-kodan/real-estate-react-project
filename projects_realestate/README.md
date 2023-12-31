025ed32f6275d447a46a0c90063ef45188163a15
Durgesh <86470225+Durgesh-kodan@users.noreply.github.com>
2023-12-31 11:59:32 +0530

    Delete projects_realestate directory

--- projects_realestate/README.md
+++
@@ -1,3 +0,0 @@
-# Real Estate App
-
-![Real Estate](https://i.ibb.co/jTW4bFC/image.png)
--- projects_realestate/next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  images:{
-    domains:['bayut-production.s3.eu-central-1.amazonaws.com']
-},
-}
