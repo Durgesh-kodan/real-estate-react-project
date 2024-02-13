38dda00cbf693e3fa293a5b2be6ddea207a2d0bb
Durgesh <86470225+Durgesh-kodan@users.noreply.github.com>
2024-02-14 02:26:08 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://real-estate-react-project-bice.vercel.app/api",
 });
 
 export const getAllProperties = async () => {
